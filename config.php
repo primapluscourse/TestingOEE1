2d38078d23982c7d4a28871384d4f2e387a97db7
primapluscourse <primapluscourse@gmail.com>
2026-03-06 11:41:21 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+// config.php - Letakkan di folder yang tidak bisa diakses publik
+return [
+    'supabase' => [
+        'url' => 'https://lgotsqbckryuogbfehve.supabase.co',
+        'key' => 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxnb3RzcWJja3J5dW9nYmZlaHZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI3NDk2ODksImV4cCI6MjA4ODMyNTY4OX0.oUyb5cEKR_ax-3BoDNv6oQPU6qMXwQKI58goRvfEdLo'
+    ],
+    'management' => [
+        'password' => 'admin123'
+    ]
+];
+?>
